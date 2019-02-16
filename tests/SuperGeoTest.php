9faccaebb0343faad5e192e13a627479a6e5300d
Argentum88 <argentimgipan@gmail.com>
2019-02-16 11:09:32 +0500

    test

--- tests/SuperGeoTest.php
+++ tests/SuperGeoTest.php
@@ -2,20 +2,34 @@
 
 namespace Argentum88\OOP\Tests;
 
+use Argentum88\OOP\GeoInfo\GeoInfo;
 use Argentum88\OOP\GeoInfoInterface;
-use Argentum88\OOP\IpToGeo\IpApiCom;
+use Argentum88\OOP\IpToGeoInterface;
 use Argentum88\OOP\SuperGeo;
 use PHPUnit\Framework\TestCase;
 
 class SuperGeoTest extends TestCase
 {
+    /** @var SuperGeo */
+    private $superGeo;
+
+    public function setUp(): void
+    {
+        $stub = $this->createMock(IpToGeoInterface::class);
+        $stub->method('getInfo')->willReturn(new GeoInfo('Foo', 'Bar'));
+
+        $this->superGeo = new SuperGeo($stub);
+    }
+
+    public function testIpValidation()
+    {
+        $this->expectException(\Exception::class);
+        $this->superGeo->getInfo('Bad');
+    }
+
     public function testGetInfo()
     {
-        //todo тут нужен двойник
-        $geoService = new IpApiCom();
-        $superGeo = new SuperGeo($geoService);
-
-        $this->assertInstanceOf(GeoInfoInterface::class ,$superGeo->getInfo(''));
-        $this->assertInstanceOf(GeoInfoInterface::class ,$superGeo->getInfo('8.8.8.8'));
+        $this->assertInstanceOf(GeoInfoInterface::class ,$this->superGeo->getInfo(''));
+        $this->assertInstanceOf(GeoInfoInterface::class ,$this->superGeo->getInfo('8.8.8.8'));
     }
 }
